19e59ec83a7c88a5a95690ec3ae4aab36f2509ec
Daniel Rodriguez <df.rodriguez143@gmail.com>
2013-03-12 02:11:01 -0500

    new official version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name='copper',
-    version='0.0.2',
+    version='0.0.3',
     author='Daniel Rodriguez',
     author_email='df.rodriguez143@gmail.com',
-    packages=['copper', 'copper.core', 'copper.io', 'copper.viz', 'copper.test'],
+    packages=['copper', 'copper.core', 'copper.utils', 'copper.viz', 'copper.tests'],
     url='http://pypi.python.org/pypi/Copper/',
     license='LICENSE.txt',
     description='Tools for doing data analysis, exploration and machine learning in python using pandas and scikit-learn. Graphics in matplotlib and D3.js',
