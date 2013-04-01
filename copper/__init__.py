8b9a0688d87b78250369d8bb0b7beab346c837be
Daniel Rodriguez <df.rodriguez143@gmail.com>
2013-03-31 23:20:15 -0500

    more r utils outside of core

--- copper/__init__.py
+++ copper/__init__.py
@@ -2,13 +2,13 @@ import os
 from copper.core.io import *
 from copper.core.set import *
 from copper.core.compare import *
-import copper.core.r as r
 
 import copper.viz.base as plot
 
 import copper.utils.transforms as transform
 import copper.utils.frame
 import copper.utils.ml
+import copper.utils.r
 
 from copper.core.config import Project
 project = Project()
