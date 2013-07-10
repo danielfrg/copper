b9442ce317092ae49e57af3df55401fa7dceb87b
Daniel Rodriguez <drodriguez@oculus360.us>
2013-07-10 16:13:17 -0500

    Better class hierarchy

--- copper/tests/test_compare_metrics_manual.py
+++ copper/tests/test_compare_metrics_manual.py
@@ -1,13 +1,8 @@
 from __future__ import division
-import math
-import random
 import copper
 import numpy as np
 import pandas as pd
-from sklearn import cross_validation
-from sklearn.linear_model import LogisticRegression
 
-from nose.tools import raises, ok_
 from copper.tests.utils import eq_
 
 
@@ -21,24 +16,30 @@ def get_train():
     ds.role['target'] = ds.TARGET
     return ds
 
+
 class BaseFake():
+
     def fit(self, X, t):
         pass
 
     def predict(self, X):
         return np.array(self.encode)
 
+
 class FakePerfect(BaseFake):
     labels = ['b', 'z', 'b', 'g', 'g', 'z', 'b', 'z', 'g', 'b', 'g', 'z']
-    encode = [ 0,   2,   0,   1,   1,   2,   0,   2,   1,   0,   1,   2]
+    encode = [0,   2,   0,   1,   1,   2,   0,   2,   1,   0,   1,   2]
+
 
 class Fake1(BaseFake):
     labels = ['g', 'z', 'b', 'z', 'g', 'z', 'b', 'b', 'g', 'b', 'g', 'z']
-    encode = [ 1,   2,   0,   2,   1,   2,   0,   0,   1,   0,   1,   2]
+    encode = [1,   2,   0,   2,   1,   2,   0,   0,   1,   0,   1,   2]
+
 
 class Fake2(BaseFake):
     labels = ['g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'b', 'g', 'g', 'g']
-    encode = [ 1,   1,   1,   1,   1,   1,   1,   1,   0,   1,   1,   1]
+    encode = [1,   1,   1,   1,   1,   1,   1,   1,   0,   1,   1,   1]
+
 
 def get_mc():
     mc = copper.ModelComparison()
@@ -58,9 +59,11 @@ def test_classes():
     mc = get_mc()
     eq_(mc.le.classes_, np.array(['b', 'g', 'z']))
 
+
 def test_target_values():
     mc = get_mc()
     eq_(mc.y_train, np.array([0, 2, 0, 1, 1, 2, 0, 2, 1, 0, 1, 2]))
+
 
 def test_accuracy():
     mc = get_mc()
@@ -68,6 +71,7 @@ def test_accuracy():
     eq_(scores['perf'], 1.0)
     eq_(scores['f1'], 0.75)
     eq_(scores['f2'], 0.25)
+
 
 def test_f1_score_avg_none():
     mc = get_mc()
