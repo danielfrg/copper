2bd19a1dbe0ccab94622687030b926f8f348478e
Daniel Rodriguez <drodriguez@oculus360.us>
2013-08-09 08:35:29 -0500

    Fixed layer weights not really mapped to same big array.

--- copper/ml/dbn/dbn.py
+++ copper/ml/dbn/dbn.py
@@ -11,24 +11,7 @@ from utils import sigmoid
 from copper.utils.progress import ProgressBar
 
 
-def assign_weights(weights, layers):
-    start_pos = 0
-    for layer in layers:
-        n_in = layer.W.shape[0]
-        n_out = layer.W.shape[1]
-
-        end_pos = start_pos + n_out
-        layer.b[:] = weights[start_pos:end_pos]
-
-        start_pos = end_pos
-        end_pos = start_pos + n_in * n_out
-        layer.W[:] = weights[start_pos:end_pos].reshape((n_in, n_out))
-        start_pos = end_pos
-
-
 def cost(weights, X, y, layers, num_labels):
-    assign_weights(weights, layers)
-
     output = layers[0].output(X)
     for layer in layers[1:]:
         output = layer.output(output)
@@ -43,8 +26,6 @@ def cost(weights, X, y, layers, num_labels):
 
 
 def cost_prime(weights, X, y, layers, num_labels):
-    assign_weights(weights, layers)
-
     Y = np.eye(num_labels)[y]
     Deltas = [np.zeros((l.n_in + 1, l.n_out)) for l in layers]
 
@@ -57,8 +38,8 @@ def cost_prime(weights, X, y, layers, num_labels):
             activations = activations + (output, )
 
         # Backprop
-        prev_delta = activations[-1] - Y[i, :].T  # last delta
-        deltas = (prev_delta, )  # deltas[0] == delta2
+        prev_delta = activations[-1] - Y[i, :].T
+        deltas = (prev_delta, )
 
         for act, layer in zip(reversed(activations[1:-1]), reversed(layers)):
             delta = np.dot(layer.W, prev_delta) * (act * (1 - act)).T
@@ -75,7 +56,8 @@ def cost_prime(weights, X, y, layers, num_labels):
 
     return np.concatenate(tuple([D.reshape(-1) for D in Deltas]))
 
-class DBN(object):
+
+class DBN(BaseEstimator):
 
     def __init__(self, hidden_layers, coef0=None, random_state=None,
                  progress_bars=False,
@@ -86,7 +68,7 @@ class DBN(object):
                  finetune_epochs=1, finetune_batches_per_epoch=-1,
                  finetune_options=None, finetune_callback=None):
         self.hidden_layers = hidden_layers
-        self._coef_ = None if coef0 is None else np.copy(coef0)
+        self.coef_ = None if coef0 is None else np.copy(coef0)
 
         if random_state is None:
             self.rnd = np.random.RandomState()
@@ -109,30 +91,38 @@ class DBN(object):
         self.finetune_options = {} if finetune_options is None else finetune_options
         self.finetune_callback = finetune_callback
 
-    def get_coef(self):
-        return self._coef_
+    def build_net(self, n_in, n_out):
+        layers = [n_in]
+        layers.extend(self.hidden_layers)
+        layers.append(n_out)
+        weights_info = [(layers[i], layers[i + 1]) for i in range(len(layers) - 1)]
 
-    def set_coef(self, n_coef):
-        self._coef_ = n_coef
-        assign_weights(n_coef, self.layers)
+        self.layers = list()
+        for w_info in weights_info:
+            n_in = w_info[0]
+            n_out = w_info[1]
+            self.layers.append(SigmoidLayer(n_in=n_in, n_out=n_out, random_state=self.rnd))
 
-    coef_ = property(get_coef, set_coef, None, "I'm the 'x' property.")
+        return weights_info
 
-
-    def rand_init(sefl, weights_info, random_state):
-        w_sizes = []
-        for layer_info in weights_info:
-            w_sizes.append((layer_info[0] + 1) * layer_info[1])
-        w_sizes[-1] = w_sizes[-1]
-
-        ans = np.zeros(sum(w_sizes))
+    def assign_weights(self):
         start_pos = 0
-        for w_size, layer_info in zip(w_sizes, weights_info):
-            end_pos = start_pos + w_size
-            gap = 4 * np.sqrt(6. / (layer_info[0] + layer_info[1]))
-            ans[start_pos:end_pos] = random_state.uniform(low=-gap, high=gap, size=w_size)
+        for layer in self.layers:
+            n_in = layer.W.shape[0]
+            n_out = layer.W.shape[1]
+
+            end_pos = start_pos + n_out
+            layer.b = self.coef_[start_pos:end_pos]
+
             start_pos = end_pos
-        return ans
+            end_pos = start_pos + n_in * n_out
+            layer.W = self.coef_[start_pos:end_pos].reshape((n_in, n_out))
+            start_pos = end_pos
+
+    def load_weights(self, filepath, n_in, n_out):
+        self.build_net(n_in, n_out)
+        self.coef_ = np.loadtxt(filepath)
+        self.assign_weights()
 
     def predict_proba(self, X):
         output = self.layers[0].output(X)
@@ -144,26 +134,24 @@ class DBN(object):
         return self.predict_proba(X).argmax(1)
 
     def fit(self, X, y):
-        layers = list(self.hidden_layers)  # Copy
-        layers.insert(0, X.shape[1])
-        layers.insert(len(layers), len(np.unique(y)))
-        weights_info = [(layers[i], layers[i + 1]) for i in range(len(layers) - 1)]
+        weights_info = self.build_net(X.shape[1], len(np.unique(y)))
 
-        self.layers = []
-        for w_info in weights_info:
-            self.layers.append(SigmoidLayer(n_in=w_info[0], n_out=w_info[1]))
-
-        if self._coef_ is None:
-            self._coef_ = self.rand_init(weights_info, self.rnd)
-            assign_weights(self._coef_, self.layers)
-
-        # Create RBM layers using the same weights
-        self.rbm_layers = []
-        for layer in self.layers:
-            self.rbm_layers.append(RBM(layer, random_state=self.rnd))
+        # Assign weights of layers as views of the big weights
+        if self.coef_ is None:
+            ws = list()
+            for layer in self.layers:
+                ws.append(layer.b.reshape(-1))
+                ws.append(layer.W.reshape(-1))
+            self.coef_ = np.concatenate(tuple(ws))
+            self.assign_weights()
 
         # Pretrain
         if self.pretrain_epochs > 0:
+            # Create RBM layers using the same weights
+            self.rbm_layers = []
+            for layer in self.layers:
+                self.rbm_layers.append(RBM(layer, random_state=self.rnd))
+
             if self.progress_bars:
                 if self.pretrain_batches_per_epoch == -1:
                     batches_per_epoch = int(X.shape[0] / self.pretrain_batch_size)
@@ -173,6 +161,7 @@ class DBN(object):
                 maxiters = self.pretrain_epochs * batches_per_epoch * len(self.layers)
                 pt_bar = ProgressBar(max=maxiters, desc='Pretrain')
 
+            # Actual pretrain
             for layer in range(len(self.rbm_layers)):
                 if layer == 0:
                     input = X
@@ -213,7 +202,7 @@ class DBN(object):
 
             self.finetune_options = self.finetune_options.copy()
             args = (self.layers, len(np.unique(y)))
-            MBOpti.minimize(self._coef_, X, y, fun=cost, grad=cost_prime, weights=self._coef_,
+            MBOpti.minimize(self.coef_, X, y, fun=cost, grad=cost_prime, weights=self.coef_,
                             method=self.finetune_method,
                             epochs=self.finetune_epochs, batch_size=self.finetune_batch_size,
                             batches_per_epoch=self.finetune_batches_per_epoch,
--- copper/ml/dbn/layers.py
+++ copper/ml/dbn/layers.py
@@ -7,14 +7,16 @@ class Layer(object):
     def __init__(self, n_in=None, n_out=None, W=None, b=None, random_state=None, activation=None):
         if random_state is None:
             self.rnd = np.random.RandomState()
+        elif isinstance(random_state, int):
+            self.rnd = np.random.RandomState(random_state)
         else:
             self.rnd = random_state
 
         if W is None and b is None:
             if n_in is not None and n_out is not None:
                 gap = 4 * np.sqrt(6. / (n_in + n_out))
-                self.W = self.rnd.uniform(low=-gap, high=gap, size=(n_in, n_out))
                 self.b = np.zeros(n_out)
+                self.W = self.rnd.uniform(low=-gap, high=gap, size=(n_in, n_out))
                 self.n_in = self.W.shape[0]
                 self.n_out = self.W.shape[1]
         else:
